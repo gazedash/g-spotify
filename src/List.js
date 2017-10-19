38d71ab302994466ecf7271ac41e6602c1ae287c
shoegazerwithak <skrtroot@protonmail.com>
2017-10-19 19:06:37 +0300

    Fix simultaneous play from both tabs

--- src/List.js
+++ src/List.js
@@ -17,27 +17,27 @@ export default class List extends Component<any, any> {
 
   handleClick = (preview_url?: string) => {
     if (this.state.url === preview_url) {
-      console.log('this,', this.howler, this.state.active);
-      if (this.howler) {
+      console.log('this,', window.howler, this.state.active);
+      if (window.howler) {
         if (!this.state.active) {
-          this.howler.play();
+          window.howler.play();
         } else {
-          this.howler.stop();
+          window.howler.stop();
         }
       } 
       this.setState({ url: preview_url, active: !this.state.active });
     } else {
       if (preview_url) {
-        console.log('that,', this.howler, this.state.active);
-        if (this.howler) {
-          this.howler.unload();
+        console.log('that,', window.howler, this.state.active);
+        if (window.howler) {
+          window.howler.unload();
         }
-        this.howler = new Howl({
+        window.howler = new Howl({
           src: [preview_url],
           format: ["mp3"]
         });
       }
-      this.howler.play();
+      window.howler.play();
       this.setState({ url: preview_url, active: true });
     }
   };
